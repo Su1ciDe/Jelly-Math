dec5df6e064a4b8e5caa7ff975172c9016a5816a
Murat Öz <murat.oz@fibergames.com.tr>
2024-05-10 13:43:54 +0300

    Added gap fillers

--- Assets/_Main/Scripts/GridSystem/Grid.cs
+++ Assets/_Main/Scripts/GridSystem/Grid.cs
@@ -16,11 +16,14 @@ namespace GridSystem
 		[Space]
 		[SerializeField] private GridNodeHolder gridNodeHolderPrefab;
 		[SerializeField] private GridNode gridNodePrefab;
+		[Space]
+		[SerializeField] private GameObject fillerPrefab;
+		[SerializeField] private GameObject fillerDotPrefab;
 
-		[SerializeField] private GridCellMatrix gridCells;
+		[SerializeField, HideInInspector] private GridCellMatrix gridCells;
 		public GridCellMatrix GridCells => gridCells;
 
-		[SerializeField] private List<GridNodeHolder> gridNodeHolders = new List<GridNodeHolder>();
+		[SerializeField, HideInInspector] private List<GridNodeHolder> gridNodeHolders = new List<GridNodeHolder>();
 		public List<GridNodeHolder> GridNodeHolders => gridNodeHolders;
 
 		#region Setup
@@ -68,18 +71,49 @@ namespace GridSystem
 				var holder = (GridNodeHolder)PrefabUtility.InstantiatePrefab(gridNodeHolderPrefab, transform);
 				holder.Setup(nodeHolderInfo.Value.Value);
 
-				foreach (var cellInfo in nodeHolderInfo.Value.Cells)
+				for (var i = 0; i < nodeHolderInfo.Value.Cells.Count; i++)
 				{
+					var cellInfo = nodeHolderInfo.Value.Cells[i];
 					var node = (GridNode)PrefabUtility.InstantiatePrefab(gridNodePrefab, holder.transform);
 					var cell = gridCells[cellInfo.Coordinates.x, cellInfo.Coordinates.y];
 					node.transform.position = cell.transform.position;
-					node.SetValue(holder.Value);
+					node.Setup(holder, holder.Value, cellInfo.Coordinates);
 					cell.CurrentNode = node;
 
 					holder.GridNodes.Add(node);
 				}
 
 				gridNodeHolders.Add(holder);
+			}
+
+			// fillers
+			foreach (var gridNodeHolder in gridNodeHolders)
+			{
+				foreach (var gridNode in gridNodeHolder.GridNodes)
+				{
+					var coor = gridNode.Coordinates;
+
+					if (coor.x + 1 < gridCells.GetLength(0) && gridCells[coor.x + 1, coor.y].CurrentNode && gridCells[coor.x + 1, coor.y].CurrentNode.ParentHolder.Equals(gridNode.ParentHolder))
+					{
+						var filler = Instantiate(fillerPrefab, transform);
+						filler.transform.localPosition = gridCells[coor.x, coor.y].transform.localPosition + .55f * Vector3.right;
+						filler.transform.localEulerAngles = 90 * Vector3.up;
+					}
+
+					if (coor.y + 1 < gridCells.GetLength(1) && gridCells[coor.x, coor.y + 1].CurrentNode && gridCells[coor.x, coor.y + 1].CurrentNode.ParentHolder.Equals(gridNode.ParentHolder))
+					{
+						var filler = Instantiate(fillerPrefab, transform);
+						filler.transform.localPosition = gridCells[coor.x, coor.y].transform.localPosition + .55f * Vector3.back;
+					}
+
+					if ((coor.x + 1 < gridCells.GetLength(0) && gridCells[coor.x + 1, coor.y].CurrentNode && gridCells[coor.x + 1, coor.y].CurrentNode.ParentHolder.Equals(gridNode.ParentHolder)) &&
+					    (coor.y + 1 < gridCells.GetLength(1) && gridCells[coor.x, coor.y + 1].CurrentNode && gridCells[coor.x, coor.y + 1].CurrentNode.ParentHolder.Equals(gridNode.ParentHolder)) &&
+					    (gridCells[coor.x + 1, coor.y + 1].CurrentNode && gridCells[coor.x + 1, coor.y + 1].CurrentNode.ParentHolder.Equals(gridNode.ParentHolder)))
+					{
+						var filler = Instantiate(fillerDotPrefab, transform);
+						filler.transform.localPosition = gridCells[coor.x, coor.y].transform.localPosition + new Vector3(0.55f, 0, -0.55f);
+					}
+				}
 			}
 		}
 #endif
@@ -117,6 +151,16 @@ namespace GridSystem
 				Arrays = new GridCellArray[index0];
 				for (int i = 0; i < index0; i++)
 					Arrays[i] = new GridCellArray(index1);
+			}
+
+			public int GetLength(int dimension)
+			{
+				return dimension switch
+				{
+					0 => Arrays.Length,
+					1 => Arrays[0].Cells.Length,
+					_ => 0
+				};
 			}
 		}
 	}
--- Assets/_Main/Scripts/GridSystem/GridNode.cs
+++ Assets/_Main/Scripts/GridSystem/GridNode.cs
@@ -6,13 +6,26 @@ namespace GridSystem
 {
 	public class GridNode : MonoBehaviour, INode
 	{
-		public GridNodeHolder ParentHolder { get; private set; }
+		[field: SerializeField, HideInInspector] public Vector2Int Coordinates { get; private set; }
+		[field: SerializeField, HideInInspector] public GridNodeHolder ParentHolder { get; private set; }
 
 		[SerializeField] private TextMeshPro txtValue;
+
+		public void Setup(GridNodeHolder parentHolder, int value, Vector2Int coor)
+		{
+			ParentHolder = parentHolder;
+			SetValue(value);
+			SetCoordinates(coor);
+		}
 
 		public void SetValue(int value)
 		{
 			txtValue.SetText(value.ToString());
+		}
+
+		public void SetCoordinates(Vector2Int coor)
+		{
+			Coordinates = coor;
 		}
 	}
 }
